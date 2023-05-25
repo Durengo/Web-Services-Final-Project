42c5b8766fb7c2eecd2da2108b80122fd4fc11bc
LastWeekNextDay <doesn@mail.ru>
2023-05-25 18:30:06 +0300

    create basis functions for weather api

--- weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/api/weatherapi/WeatherApiEndpoint.java
+++ weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/api/weatherapi/WeatherApiEndpoint.java
@@ -1,9 +1,20 @@
 package lt.viko.eif.pi21e.weather.server.rest.api.weatherapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherApiEndpoint {
+    @GetMapping("/weather?coordinates={lat},{lon}")
+    public String getWeatherCoordinates(@PathVariable float lat, @PathVariable float lon) {
+        return "Hello from weather API";
+    }
+
+    @GetMapping("/weather?city={city}")
+    public String getWeatherCity(@PathVariable String city) {
+        return "Hello from weather API";
+    }
 
 }
