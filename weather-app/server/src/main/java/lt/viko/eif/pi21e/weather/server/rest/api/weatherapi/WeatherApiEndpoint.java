d9423a7b72f8f0e0c5dc24dbee608bae90c6dc18
LastWeekNextDay <doesn@mail.ru>
2023-05-25 17:45:16 +0300

    created weather api endpoint class

---
+++ weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/api/weatherapi/WeatherApiEndpoint.java
@@ -0,0 +1,9 @@
+package lt.viko.eif.pi21e.weather.server.rest.api.weatherapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeatherApiEndpoint {
+
+}
