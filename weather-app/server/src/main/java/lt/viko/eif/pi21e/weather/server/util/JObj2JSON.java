488f68bfc8115c67a278851eb75c193fb83efa09
LastWeekNextDay <doesn@mail.ru>
2023-05-26 18:13:00 +0300

    util class for converting Java Objects and JSON

---
+++ weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/util/JObj2JSON.java
@@ -0,0 +1,15 @@
+package lt.viko.eif.pi21e.weather.server.util;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+public class JObj2JSON {
+    public static String convert(Object obj) throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(obj);
+    }
+
+    public static <T> T convert(String json, Class<T> type) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, type);
+    }
+}
