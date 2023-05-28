cf561e75c8e96390e7a7c7fbbb992506cffb04ef
LastWeekNextDay <doesn@mail.ru>
2023-05-28 19:57:55 +0300

    generalized methods + added ability to add and delete criterias to subscribed address

--- weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/database/SubscriptionAddressClient.java
+++ weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/database/SubscriptionAddressClient.java
@@ -2,126 +2,94 @@ package lt.viko.eif.pi21e.weather.server.rest.database;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lt.viko.eif.pi21e.weather.database.interactor.Interactor;
+import lt.viko.eif.pi21e.weather.database.models.CriteriaWeather;
 import lt.viko.eif.pi21e.weather.database.models.SubscriptionAddress;
-import lt.viko.eif.pi21e.weather.server.rest.responses.ResponseClass;
+import lt.viko.eif.pi21e.weather.server.rest.database.other.ClientGenericMethods;
+import lt.viko.eif.pi21e.weather.server.rest.database.other.ResponseProvider;
 import lt.viko.eif.pi21e.weather.server.util.JObj2JSON;
 
 import java.util.List;
 
-public class SubscriptionAddressClient {
-    private final String RESPONSE_ERROR = """
-                    {
-                      "status": 500,
-                      "message": "Couldn't convert response to json",
-                      "data": "NULL"
-                    }""";
-
-    private final String RESPONSE_NOT_FOUND = """
-                    {
-                      "status": 404,
-                      "message": "Not Found",
-                      "data": "NULL"
-                    }""";
-
+public class SubscriptionAddressClient extends ResponseProvider {
     public String getSubscriptionAddress(int id) {
-        SubscriptionAddress newSubscriptionAddress = Interactor.read(SubscriptionAddress.class, id);
-
-        ResponseClass response = new ResponseClass();
-        if (newSubscriptionAddress != null) {
-            response.setStatus(200);
-            response.setMessage("OK");
-            String json = "";
-            try {
-                json = JObj2JSON.convert(newSubscriptionAddress);
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert subscription address to JSON");
-                response.setData("NULL");
-            }
-            response.setData(json);
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.getX(id, SubscriptionAddress.class);
     }
 
     public String getSubscriptionAddresses() {
-        List<SubscriptionAddress> subscriptionAddresses = Interactor.readAll(SubscriptionAddress.class);
-
-        ResponseClass response = new ResponseClass();
-        if (subscriptionAddresses != null) {
-            response.setStatus(200);
-            response.setMessage("OK");
-            String json = "";
-            try {
-                json = JObj2JSON.convert(subscriptionAddresses);
-                response.setData(json);
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert subscription addresses to JSON");
-                response.setData("NULL");
-            }
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.getXs(SubscriptionAddress.class);
     }
 
     public String createSubscriptionAddress(String subscriptionAddressJson) {
-        try {
-            SubscriptionAddress newSubscriptionAddress = null;
-            ResponseClass response = new ResponseClass();
-            try {
-                newSubscriptionAddress = JObj2JSON.convert(subscriptionAddressJson, SubscriptionAddress.class);
-                Interactor.set(newSubscriptionAddress);
-                SubscriptionAddress check = Interactor.read(SubscriptionAddress.class, newSubscriptionAddress.getSubscriptionAddressId());
-                if (check != null) {
-                    response.setStatus(201);
-                    response.setMessage("Created");
-                    response.setData(JObj2JSON.convert(check));
-                } else {
-                    response.setStatus(500);
-                    response.setMessage("Couldn't create subscription address");
-                    response.setData("NULL");
-                }
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert subscription address to JSON");
-                response.setData("NULL");
-            }
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.createX(subscriptionAddressJson, SubscriptionAddress.class);
     }
 
     public String deleteSubscriptionAddress(int id) {
-        SubscriptionAddress existingSubscriptionAddress = Interactor.read(SubscriptionAddress.class, id);
+        return ClientGenericMethods.deleteX(id, SubscriptionAddress.class);
+    }
 
-        ResponseClass response = new ResponseClass();
-        if (existingSubscriptionAddress != null) {
-            Interactor.delete(SubscriptionAddress.class, id);
-            response.setStatus(200);
-            response.setMessage("Subscription Address Deleted");
-            response.setData("NULL");
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
+    public String addCriteriaWeather(int subId, String criteriaWeatherJson) {
         try {
-            return JObj2JSON.convert(response);
+            SubscriptionAddress existingSubscriptionAddress = Interactor.read(SubscriptionAddress.class, subId);
+            if (existingSubscriptionAddress != null) {
+                CriteriaWeather criteriaWeather = JObj2JSON.convert(criteriaWeatherJson, CriteriaWeather.class);
+                criteriaWeather.setSubscriptionAddress(existingSubscriptionAddress);
+                List<CriteriaWeather> collection = existingSubscriptionAddress.getCriteriaWeathers();
+                collection.add(criteriaWeather);
+                existingSubscriptionAddress.setCriteriaWeathers(collection);
+                Interactor.update(existingSubscriptionAddress);
+                SubscriptionAddress check = Interactor.read(SubscriptionAddress.class, subId);
+                boolean found = false;
+                if (check != null) {
+                    for (CriteriaWeather criteriaWeather1 : check.getCriteriaWeathers()) {
+                        if (criteriaWeather1.getLess_equal_more().equals(criteriaWeather.getLess_equal_more()) &&
+                                criteriaWeather1.getCriteriaName().equals(criteriaWeather.getCriteriaName()) &&
+                                criteriaWeather1.getCriteriaValue().equals(criteriaWeather.getCriteriaValue())) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        return getResponse(200, "OK", JObj2JSON.convert(check));
+                    } else {
+                        return getResponse(500, "Couldn't add criteria weather", "NULL");
+                    }
+                } else {
+                    return getResponse(500, "Subscribed address was destroyed", "NULL");
+                }
+            } else {
+                return getResponse(404, "Not Found", "NULL");
+            }
         } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
+            return getResponse(500, "Couldn't convert CriteriaWeather to JSON", e.getMessage());
         }
     }
+
+    public String deleteCriteriaWeather(int subId, int criteriaWeatherId) {
+        try {
+            SubscriptionAddress subscriptionAddress = Interactor.read(SubscriptionAddress.class, subId);
+            if (subscriptionAddress != null) {
+                List<CriteriaWeather> criteriaWeathers = subscriptionAddress.getCriteriaWeathers();
+                CriteriaWeather criteriaToRemove = null;
+                for (CriteriaWeather criteriaWeather : criteriaWeathers) {
+                    if (criteriaWeather.getCriteriaWeatherId() == criteriaWeatherId) {
+                        criteriaToRemove = criteriaWeather;
+                        break;
+                    }
+                }
+                if (criteriaToRemove != null) {
+                    criteriaWeathers.remove(criteriaToRemove);
+                    subscriptionAddress.setCriteriaWeathers(criteriaWeathers);
+                    Interactor.update(subscriptionAddress);
+                    return getResponse(200, "OK", "Criteria weather successfully removed");
+                } else {
+                    return getResponse(404, "Criteria Weather not found", "NULL");
+                }
+            } else {
+                return getResponse(404, "Subscription Address not found", "NULL");
+            }
+        } catch (Exception e) {
+            return getResponse(500, "Couldn't delete Criteria Weather", e.getMessage());
+        }
+    }
+
 }
