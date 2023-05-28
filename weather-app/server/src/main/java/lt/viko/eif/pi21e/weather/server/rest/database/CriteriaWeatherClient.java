6af971498e20427dc17db6c388fc82c0d91d5b85
LastWeekNextDay <doesn@mail.ru>
2023-05-28 19:58:12 +0300

    generalized where possible

--- weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/database/CriteriaWeatherClient.java
+++ weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/database/CriteriaWeatherClient.java
@@ -3,126 +3,26 @@ package lt.viko.eif.pi21e.weather.server.rest.database;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lt.viko.eif.pi21e.weather.database.interactor.Interactor;
 import lt.viko.eif.pi21e.weather.database.models.CriteriaWeather;
-import lt.viko.eif.pi21e.weather.server.rest.responses.ResponseClass;
+import lt.viko.eif.pi21e.weather.server.rest.database.other.ClientGenericMethods;
+import lt.viko.eif.pi21e.weather.server.rest.database.other.ResponseProvider;
 import lt.viko.eif.pi21e.weather.server.util.JObj2JSON;
 
 import java.util.List;
 
 public class CriteriaWeatherClient {
-
-    private final String RESPONSE_ERROR = """
-                    {
-                      "status": 500,
-                      "message": "Couldn't convert response to json",
-                      "data": "NULL"
-                    }""";
-
-    private final String RESPONSE_NOT_FOUND = """
-                    {
-                      "status": 404,
-                      "message": "Not Found",
-                      "data": "NULL"
-                    }""";
-
     public String getCriteriaWeather(int id) {
-        CriteriaWeather criteriaWeather = Interactor.read(CriteriaWeather.class, id);
-
-        ResponseClass response = new ResponseClass();
-        if (criteriaWeather != null) {
-            response.setStatus(200);
-            response.setMessage("OK");
-            String json = "";
-            try {
-                json = JObj2JSON.convert(criteriaWeather);
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert CriteriaWeather to JSON");
-                response.setData("NULL");
-            }
-            response.setData(json);
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.getX(id, CriteriaWeather.class);
     }
 
     public String getCriteriaWeathers() {
-        List<CriteriaWeather> criteriaWeathers = Interactor.readAll(CriteriaWeather.class);
-
-        ResponseClass response = new ResponseClass();
-        if (criteriaWeathers != null) {
-            response.setStatus(200);
-            response.setMessage("OK");
-            String json = "";
-            try {
-                json = JObj2JSON.convert(criteriaWeathers);
-                response.setData(json);
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert CriteriaWeathers to JSON");
-                response.setData("NULL");
-            }
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.getXs(CriteriaWeather.class);
     }
 
     public String createCriteriaWeather(String criteriaWeatherJson) {
-        try {
-            CriteriaWeather newCriteriaWeather = null;
-            ResponseClass response = new ResponseClass();
-            try {
-                newCriteriaWeather = JObj2JSON.convert(criteriaWeatherJson, CriteriaWeather.class);
-                Interactor.set(newCriteriaWeather);
-                CriteriaWeather check = Interactor.read(CriteriaWeather.class, newCriteriaWeather.getCriteriaWeatherId());
-                if (check != null) {
-                    response.setStatus(201);
-                    response.setMessage("Created");
-                    response.setData(JObj2JSON.convert(check));
-                } else {
-                    response.setStatus(500);
-                    response.setMessage("Couldn't create CriteriaWeather");
-                    response.setData("NULL");
-                }
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert CriteriaWeather to JSON");
-                response.setData("NULL");
-            }
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.createX(criteriaWeatherJson, CriteriaWeather.class);
     }
 
     public String deleteCriteriaWeather(int id) {
-        CriteriaWeather existingCriteriaWeather = Interactor.read(CriteriaWeather.class, id);
-
-        ResponseClass response = new ResponseClass();
-        if (existingCriteriaWeather != null) {
-            Interactor.delete(CriteriaWeather.class, id);
-            response.setStatus(200);
-            response.setMessage("OK");
-            response.setData("NULL");
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.deleteX(id, CriteriaWeather.class);
     }
 }
--- weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/database/FavoriteAddressClient.java
+++ weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/rest/database/FavoriteAddressClient.java
@@ -3,126 +3,26 @@ package lt.viko.eif.pi21e.weather.server.rest.database;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lt.viko.eif.pi21e.weather.database.interactor.Interactor;
 import lt.viko.eif.pi21e.weather.database.models.FavoriteAddress;
-import lt.viko.eif.pi21e.weather.server.rest.responses.ResponseClass;
+import lt.viko.eif.pi21e.weather.server.rest.database.other.ClientGenericMethods;
+import lt.viko.eif.pi21e.weather.server.rest.database.other.ResponseProvider;
 import lt.viko.eif.pi21e.weather.server.util.JObj2JSON;
 
 import java.util.List;
 
-public class FavoriteAddressClient {
-
-    private final String RESPONSE_ERROR = """
-                    {
-                      "status": 500,
-                      "message": "Couldn't convert response to json",
-                      "data": "NULL"
-                    }""";
-
-    private final String RESPONSE_NOT_FOUND = """
-                    {
-                      "status": 404,
-                      "message": "Not Found",
-                      "data": "NULL"
-                    }""";
-
+public class FavoriteAddressClient extends ResponseProvider {
     public String getFavoriteAddress(int id) {
-        FavoriteAddress favoriteAddress = Interactor.read(FavoriteAddress.class, id);
-
-        ResponseClass response = new ResponseClass();
-        if (favoriteAddress != null) {
-            response.setStatus(200);
-            response.setMessage("OK");
-            String json = "";
-            try {
-                json = JObj2JSON.convert(favoriteAddress);
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert FavoriteAddress to JSON");
-                response.setData("NULL");
-            }
-            response.setData(json);
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.getX(id, FavoriteAddress.class);
     }
 
     public String getFavoriteAddresses() {
-        List<FavoriteAddress> favoriteAddresses = Interactor.readAll(FavoriteAddress.class);
-
-        ResponseClass response = new ResponseClass();
-        if (favoriteAddresses != null) {
-            response.setStatus(200);
-            response.setMessage("OK");
-            String json = "";
-            try {
-                json = JObj2JSON.convert(favoriteAddresses);
-                response.setData(json);
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert FavoriteAddresses to JSON");
-                response.setData("NULL");
-            }
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.getXs(FavoriteAddress.class);
     }
 
     public String createFavoriteAddress(String favoriteAddressJson) {
-        try {
-            FavoriteAddress newFavoriteAddress = null;
-            ResponseClass response = new ResponseClass();
-            try {
-                newFavoriteAddress = JObj2JSON.convert(favoriteAddressJson, FavoriteAddress.class);
-                Interactor.set(newFavoriteAddress);
-                FavoriteAddress check = Interactor.read(FavoriteAddress.class, newFavoriteAddress.getFavoriteAddressId());
-                if (check != null) {
-                    response.setStatus(201);
-                    response.setMessage("Created");
-                    response.setData(JObj2JSON.convert(check));
-                } else {
-                    response.setStatus(500);
-                    response.setMessage("Couldn't create FavoriteAddress");
-                    response.setData("NULL");
-                }
-            } catch (JsonProcessingException e) {
-                response.setStatus(500);
-                response.setMessage("Couldn't convert FavoriteAddress to JSON");
-                response.setData("NULL");
-            }
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.createX(favoriteAddressJson, FavoriteAddress.class);
     }
 
     public String deleteFavoriteAddress(int id) {
-        FavoriteAddress existingFavoriteAddress = Interactor.read(FavoriteAddress.class, id);
-
-        ResponseClass response = new ResponseClass();
-        if (existingFavoriteAddress != null) {
-            Interactor.delete(FavoriteAddress.class, id);
-            response.setStatus(200);
-            response.setMessage("OK");
-            response.setData("NULL");
-        } else {
-            return RESPONSE_NOT_FOUND;
-        }
-
-        try {
-            return JObj2JSON.convert(response);
-        } catch (JsonProcessingException e) {
-            return RESPONSE_ERROR;
-        }
+        return ClientGenericMethods.deleteX(id, FavoriteAddress.class);
     }
 }
