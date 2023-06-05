722eeda7653442b3e1d876bce63695463f79c9b0
WiciokS <77240194+WiciokS@users.noreply.github.com>
2023-06-05 21:24:48 +0300

    Some new classes

---
+++ weather-app/server/src/main/java/lt/viko/eif/pi21e/weather/server/ActiveMQ/MessageReceiver.java
@@ -0,0 +1,45 @@
+package lt.viko.eif.pi21e.weather.server.ActiveMQ;
+
+import javax.jms.*;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class MessageReceiver {
+    private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+    private ConnectionFactory connectionFactory;
+    private Connection connection;
+    private Session session;
+    private MessageConsumer consumer;
+
+    public MessageReceiver(String queueName) throws JMSException {
+        connectionFactory = new ActiveMQConnectionFactory(url);
+        connection = connectionFactory.createConnection();
+        connection.start();
+
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createQueue(queueName);
+
+        consumer = session.createConsumer(destination);
+    }
+
+    public void receiveMessage() throws JMSException {
+        Message message = consumer.receive();
+
+        if (message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+            System.out.println("Received message: " + textMessage.getText());
+        }
+    }
+
+    public void close() throws JMSException {
+        connection.close();
+    }
+
+    public static void main(String[] args) throws JMSException {
+        MessageReceiver receiver = new MessageReceiver("-1");
+        receiver.receiveMessage();
+        receiver.close();
+    }
+}
+
+
