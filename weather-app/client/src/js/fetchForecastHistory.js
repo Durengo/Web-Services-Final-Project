3d6f682ba2fe6f22b6e8d2a34259aff36f230a7f
Sebastian Termen <Sebastian.Termen@gmail.com>
2023-06-11 15:55:53 +0300

    Changed fetching of forecast history, which will only be called once when the app starts up. There is a method for when the IP location has already been called. The other method will be called when coordinates on the map are changed.

--- weather-app/client/src/js/fetchForecastHistory.js
+++ weather-app/client/src/js/fetchForecastHistory.js
@@ -2,20 +2,49 @@ import axios, {get} from "axios";
 import {LOCAL_API_URL} from "../env/env";
 import {getClientIP} from "./getClientIP";
 import {setForecastHistory, setIsFetchingForecastHistory} from "../redux/actions/forecastHistoryActions";
+import {fetchCurrentLocationByIp} from "./fetchCurrentLocation";
+import store from "../redux/store";
 
-export const fetchForecastHistory = (cityName, startDate, endDate) => {
+export const fetchForecastHistoryByIp = (startDate, endDate) => {
     return async (dispatch) => {
         dispatch(setIsFetchingForecastHistory(true)); // Set the fetching flag to true to indicate the state is being fetched
-        if(!cityName)
+        if(!startDate || !endDate)
         {
             dispatch(setIsFetchingForecastHistory(false));
-            console.log("Canceling dispatch city name is null.");
+            console.log("Canceling dispatch start or end date is null.");
             return;
         }
-        console.log(`${LOCAL_API_URL}/history/` + cityName + "?start=" + startDate, + "&end=" + endDate);
+        console.log("REQUEST: " + `${LOCAL_API_URL}/history/coordinates?start=` + startDate +"&end=" + endDate);
         try {
-            const ip = await getClientIP();
+            // const ip = await dispatch(fetchCurrentLocationByIp());
+            const state = await store.getState();
+            const cityName = state.currentLocation.city.name;
+            console.log(store.getState());
             const response = await axios.get(`${LOCAL_API_URL}/history/` + cityName + "?start=" + startDate + "&end=" + endDate); // Perform the asynchronous operation to fetch the location
+            const forecast = response.data; // Extract the location data from the response
+            console.log("Retrieved Data! Current Forecast History: ", forecast);
+            dispatch(setForecastHistory(forecast)); // Dispatch an action to set the fetched location state in Redux
+        } catch (error) {
+            console.error("Error fetching weather information:", error);
+            dispatch(setForecastHistory(null)); // Set the fetching flag to false in case of an error
+        } finally {
+            dispatch(setIsFetchingForecastHistory(false));
+        }
+    };
+};
+
+export const fetchForecastHistoryByCoordinates = (latitude, longitude, startDate, endDate) => {
+    return async (dispatch) => {
+        dispatch(setIsFetchingForecastHistory(true)); // Set the fetching flag to true to indicate the state is being fetched
+        if(!latitude || !longitude)
+        {
+            dispatch(setIsFetchingForecastHistory(false));
+            console.log("Canceling dispatch latitude or longitude is null.");
+            return;
+        }
+        console.log("REQUEST: " + `${LOCAL_API_URL}/history/coordinates?lat=` + latitude + "&lon=" + longitude + "&start=" + startDate +"&end=" + endDate);
+        try {
+            const response = await axios.get(`${LOCAL_API_URL}/history/coordinates?lat=` + latitude + "&lon=" + longitude + "&start=" + startDate +"&end=" + endDate); // Perform the asynchronous operation to fetch the location
             const forecast = response.data; // Extract the location data from the response
             console.log("Retrieved Data! Current Forecast History: ", forecast);
             dispatch(setForecastHistory(forecast)); // Dispatch an action to set the fetched location state in Redux
