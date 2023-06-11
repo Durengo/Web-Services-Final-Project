3d00e4a9d8cf269f1e13ea98434d90f09ee84d48
Sebastian Termen <Sebastian.Termen@gmail.com>
2023-06-11 15:59:47 +0300

    Actions for modifying map coordinates data.

--- weather-app/client/src/redux/actions/mapCoordinatesActions.js
+++ weather-app/client/src/redux/actions/mapCoordinatesActions.js
@@ -1,13 +1,31 @@
 export const SET_MAP_COORDINATES = "SET_MAP_COORDINATES";
+export const SET_PREVIOUS_MAP_COORDINATES = "SET_PREVIOUS_MAP_COORDINATES";
 export const SET_IS_FETCHING_MAP_COORDINATES = "SET_IS_FETCHING_MAP_COORDINATES";
-
-export const setMapCoordinates = (mapCoordinates) => {
+export const SET_MAP_COORDINATES_FIRST_TIME = "SET_MAP_COORDINATES_FIRST_TIME";
+export const setMapCoordinates = (latitude, longitude) => {
     return {
         type: SET_MAP_COORDINATES,
-        payload: mapCoordinates,
+        payload: {
+            latitude: latitude,
+            longitude: longitude,
+        },
     };
 };
-
+export const setPreviousMapCoordinates = (latitude, longitude) => {
+    return {
+        type: SET_PREVIOUS_MAP_COORDINATES,
+        payload: {
+            latitude: latitude,
+            longitude: longitude,
+        },
+    };
+};
+export const setMapCoordinatesFirstTime = (mapCoordinatesFirstTime) => {
+    return {
+        type: SET_MAP_COORDINATES_FIRST_TIME,
+        payload: mapCoordinatesFirstTime,
+    };
+};
 export const setIsFetchingMapCoordinates = (isFetchingMapCoordinates) => {
     return {
         type: SET_IS_FETCHING_MAP_COORDINATES,
--- weather-app/client/src/redux/reducer.js
+++ weather-app/client/src/redux/reducer.js
@@ -1,13 +1,31 @@
 import {initialState} from "./initialStates";
-import {SET_PRESSURE_UNIT, SET_VISIBILITY_UNIT, SET_WIND_SPEED_UNIT} from "./actions/weatherUnits";
+import {
+    SET_PRECIPITATION_UNIT,
+    SET_PRESSURE_UNIT,
+    SET_TEMPERATURE_UNIT,
+    SET_VISIBILITY_UNIT,
+    SET_WIND_SPEED_UNIT
+} from "./actions/weatherUnits";
+import {SET_CURRENT_LOCATION, SET_IS_FETCHING_LOCATION} from "./actions/currentLocationActions";
+import {SET_IS_FETCHING_WEATHER_INFORMATION, SET_WEATHER_INFORMATION} from "./actions/weatherInformationActions";
+import {SET_CURRENT_USER, SET_IS_FETCHING_CURRENT_USER} from "./actions/sessionUser";
+import {
+    SET_IS_UPDATING_SESSION_DATE_TODAY,
+    SET_SESSION_DATE_TODAY,
+    SET_SESSION_IP,
+    SET_SESSION_LOGGED_IN,
+    SET_SESSION_USING_IP
+} from "./actions/session";
+import {SET_FORECAST_HISTORY, SET_IS_FETCHING_FORECAST_HISTORY} from "./actions/forecastHistoryActions";
+import {
+    SET_IS_FETCHING_MAP_COORDINATES,
+    SET_MAP_COORDINATES,
+    SET_MAP_COORDINATES_FIRST_TIME, SET_PREVIOUS_MAP_COORDINATES
+} from "./actions/mapCoordinatesActions";
+
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case "SET_DATA":
-            return {
-                ...state,
-                data: action.payload,
-            };
-        case "SET_CURRENT_LOCATION":
+        case SET_CURRENT_LOCATION:
             return {
                 ...state,
                 currentLocation: {
@@ -15,12 +33,12 @@ export const reducer = (state = initialState, action) => {
                     ...action.payload,
                 },
             };
-        case "SET_IS_FETCHING_CURRENT_LOCATION":
+        case SET_IS_FETCHING_LOCATION:
             return {
                 ...state,
                 isFetchingCurrentLocation: action.payload,
             };
-        case "SET_WEATHER_INFORMATION":
+        case SET_WEATHER_INFORMATION:
             return {
                 ...state,
                 weatherInformation: {
@@ -28,37 +46,37 @@ export const reducer = (state = initialState, action) => {
                     ...action.payload,
                 },
             };
-        case "SET_IS_FETCHING_WEATHER_INFORMATION":
+        case SET_IS_FETCHING_WEATHER_INFORMATION:
             return {
                 ...state,
                 isFetchingWeatherInformation: action.payload,
             };
-        case "SET_TEMPERATURE_UNIT":
+        case SET_TEMPERATURE_UNIT:
             return {
                 ...state,
                 temperatureUnit: action.payload,
             };
-        case "SET_WIND_SPEED_UNIT":
+        case SET_WIND_SPEED_UNIT:
             return {
                 ...state,
                 windSpeedUnit: action.payload,
             };
-        case "SET_VISIBILITY_UNIT":
+        case SET_VISIBILITY_UNIT:
             return {
                 ...state,
                 visibilityUnit: action.payload,
             };
-        case "SET_PRECIPITATION_UNIT":
+        case SET_PRECIPITATION_UNIT:
             return {
                 ...state,
                 precipitationUnit: action.payload,
             };
-        case "SET_PRESSURE_UNIT":
+        case SET_PRESSURE_UNIT:
             return {
                 ...state,
                 pressureUnit: action.payload,
             };
-        case "SET_CURRENT_USER":
+        case SET_CURRENT_USER:
             return {
                 ...state,
                 // currentUser: action.payload,
@@ -67,17 +85,37 @@ export const reducer = (state = initialState, action) => {
                     ...action.payload,
                 }
             };
-        case "SET_IS_FETCHING_CURRENT_USER":
+        case SET_IS_FETCHING_CURRENT_USER:
             return {
                 ...state,
                 isFetchingCurrentUser: action.payload,
             };
-        case "SET_SESSION_LOGGED_IN":
+        case SET_SESSION_LOGGED_IN:
             return {
                 ...state,
                 session: action.payload,
             };
-        case "SET_FORECAST_HISTORY":
+        case SET_SESSION_IP:
+            return {
+                ...state,
+                sessionIp: action.payload,
+            };
+        case SET_SESSION_DATE_TODAY:
+            return {
+                ...state,
+                sessionDateToday: action.payload,
+            };
+        case SET_IS_UPDATING_SESSION_DATE_TODAY:
+            return {
+                ...state,
+                isUpdatingSessionDateToday: action.payload,
+            };
+        case SET_SESSION_USING_IP:
+            return {
+                ...state,
+                sessionUsingIp: action.payload,
+            };
+        case SET_FORECAST_HISTORY:
             return {
                 ...state,
                 forecastHistory: {
@@ -85,11 +123,31 @@ export const reducer = (state = initialState, action) => {
                     ...action.payload,
                 },
             };
-        case "SET_IS_FETCHING_FORECAST_HISTORY":
+        case SET_IS_FETCHING_FORECAST_HISTORY:
             return {
                 ...state,
                 isFetchingForecastHistory: action.payload,
             };
+        case SET_MAP_COORDINATES:
+            return {
+                ...state,
+                mapCoordinates: action.payload,
+            };
+        case SET_PREVIOUS_MAP_COORDINATES:
+            return {
+                ...state,
+                previousMapCoordinates: action.payload,
+            };
+        case SET_IS_FETCHING_MAP_COORDINATES:
+            return {
+              ...state,
+              isFetchingMapCoordinates: action.payload,
+            };
+        case SET_MAP_COORDINATES_FIRST_TIME:
+            return {
+                ...state,
+                mapCoordinatesFirstTime: action.payload,
+            }
         default:
             return state;
     }
