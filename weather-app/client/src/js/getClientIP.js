a1f5947573330ff66b9386694069df13607f3da3
Sebastian Termen <Sebastian.Termen@gmail.com>
2023-06-11 15:56:39 +0300

    Added the static API URL to getClientIP method.

--- weather-app/client/src/js/getClientIP.js
+++ weather-app/client/src/js/getClientIP.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import {IP_API_2} from "../env/env";
 
 // Getting the IP of the client by using the following API: https://ipapi.co/
 
 export const getClientIP = async () => {
     try {
-        const response = await axios.get("https://ipapi.co/json");
+        const response = await axios.get(IP_API_2);
         const data = response.data;
         const ipAddress = data.ip;
         console.log("Client IP: ", ipAddress);
