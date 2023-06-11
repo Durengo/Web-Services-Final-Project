66d747402978c0a5a7b7d2414964792280a220d4
Sebastian Termen <Sebastian.Termen@gmail.com>
2023-06-12 00:47:05 +0300

    Componentized DayForecast. Added new methods for getting forecast history.

--- weather-app/client/src/components/MonthlyWeekly/index.jsx
+++ weather-app/client/src/components/MonthlyWeekly/index.jsx
@@ -1,162 +1,138 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import "./X2.css";
 import DetailsComponent from "../Details";
 import {weatherMainData} from "../../js/App";
 import CurrentConditionsComponent from "../CurrentConditions";
+import {useDispatch, useSelector} from "react-redux";
+import {setDateToday} from "../../js/setDateToday";
+import {setSessionUsingIp} from "../../redux/actions/session";
+import {fetchWeatherInformationByCoordinates, fetchWeatherInformationByIP} from "../../js/fetchWeatherInformation";
+import DayForecast from "../DayForecast";
 import WeatherGrid from "../ForecastTable";
+import {
+    setMapCoordinates,
+    setMapCoordinatesFirstTime,
+    setPreviousMapCoordinates
+} from "../../redux/actions/mapCoordinatesActions";
+import {fetchForecastHistoryByCoordinatesAhead} from "../../js/fetchForecastHistory";
 
 function X2(props) {
+    const dispatch = useDispatch();
+    const isMountedRef = useRef(false);
+
+    // Setting Date For States
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    const formattedDate = `${year}-${month}-${day}`;
+
+    const currentLocation = useSelector((state) => state.currentLocation);
+    const weatherInformation = useSelector((state) => state.weatherInformation);
+
+    const sessionIp = useSelector((state) => state.sessionIp);
+    const sessionDateToday = useSelector((state) => state.sessionDateToday);
+    const isUpdatingSessionDateToday = useSelector((state) => state.isUpdatingSessionDateToday);
+    const sessionUsingIp = useSelector((state) => state.sessionUsingIp);
+
+    const mapCoordinates = useSelector((state) => state.mapCoordinates);
+    const previousMapCoordinates = useSelector((state) => state.previousMapCoordinates);
+    const mapCoordinatesFirstTime = useSelector((state) => state.mapCoordinatesFirstTime);
+    const isFetchingMapCoordinates = useSelector((state) => state.isFetchingMapCoordinates);
+
+    const isFetchingCurrentLocation = useSelector((state) => state.isFetchingCurrentLocation);
+    const isFetchingWeatherInformation = useSelector((state) => state.isFetchingWeatherInformation);
+
+    const calledOnce = useRef(false);
+
+    const forecastHistory = useSelector((state) => state.forecastHistory);
+    const isFetchingForecastHistory = useSelector((state) => state.isFetchingForecastHistory);
+    const temperatureUnit = useSelector((state) => state.temperatureUnit);
+    const sessionTodayDate = useSelector((state) => state.sessionDateToday);
+
+    useEffect(() => {
+        if (!isMountedRef.current) {
+            dispatch(setDateToday());
+            dispatch(setSessionUsingIp(false));
+            isMountedRef.current = true;
+        }
+    }, []);
+
+    useEffect(() => {
+        if (sessionUsingIp) {
+            dispatch(fetchWeatherInformationByIP());
+            dispatch(setSessionUsingIp(false));
+        }
+    }, [sessionUsingIp]);
+
+    useEffect(() => {
+        if (weatherInformation && !isFetchingCurrentLocation && !isFetchingWeatherInformation) {
+            if ((previousMapCoordinates !== null && mapCoordinates !== null)) {
+                dispatch(setMapCoordinates(weatherInformation.location.lat, weatherInformation.location.lon));
+                dispatch(setPreviousMapCoordinates(weatherInformation.location.lat, weatherInformation.location.lon));
+            } else if ((previousMapCoordinates.latitude !== mapCoordinates.latitude)
+                && (previousMapCoordinates.longitude !== mapCoordinates.longitude)) {
+                dispatch(setPreviousMapCoordinates(mapCoordinates.latitude, mapCoordinates.longitude));
+                dispatch(setMapCoordinates(weatherInformation.location.lat, weatherInformation.location.lon));
+            }
+        }
+    }, [weatherInformation, isFetchingCurrentLocation, isFetchingWeatherInformation]);
+
+    useEffect(() => {
+        if (mapCoordinatesFirstTime) {
+            dispatch(fetchWeatherInformationByCoordinates(mapCoordinates.latitude, mapCoordinates.longitude));
+            dispatch(setMapCoordinatesFirstTime(false));
+        }
+    }, [mapCoordinatesFirstTime]);
+
+    useEffect(() => {
+        if (mapCoordinates && !isFetchingMapCoordinates) {
+            console.log(`New Map Coordinates: ${mapCoordinates.latitude}:${mapCoordinates.longitude}`);
+            console.log(`Previous Map Coordinates: ${previousMapCoordinates.latitude}:${previousMapCoordinates.longitude}`);
+            if (mapCoordinatesFirstTime) {
+                dispatch(fetchWeatherInformationByCoordinates(mapCoordinates.latitude, mapCoordinates.longitude));
+                dispatch(setMapCoordinatesFirstTime(false));
+            }
+        }
+    }, [mapCoordinates, isFetchingMapCoordinates]);
+
+    useEffect(() => {
+        if (sessionDateToday && !isUpdatingSessionDateToday) {
+            console.log(`Today day set to: ${sessionDateToday.year}-${sessionDateToday.month}-${sessionDateToday.day}`);
+        }
+    }, [sessionDateToday, isUpdatingSessionDateToday]);
+
+    useEffect(() => {
+        if (weatherInformation && sessionDateToday && !isFetchingWeatherInformation && !isUpdatingSessionDateToday) {
+            if (sessionUsingIp) {
+                //dispatch(fetchForecastHistoryByIp(`${formattedDate}`, `${formattedDateAhead}`));
+            } else {
+                dispatch(fetchForecastHistoryByCoordinatesAhead(mapCoordinates.latitude, mapCoordinates.longitude, `${formattedDate}`, `${formattedDate}`));
+                // dispatch(fetchForecastHistoryByCoordinates(mapCoordinates.latitude, mapCoordinates.longitude, `${formattedDate}`, `${formattedDateAhead}`));
+            }
+        }
+    }, [dispatch, weatherInformation, isFetchingWeatherInformation, sessionDateToday, isUpdatingSessionDateToday]);
+
+    if (isFetchingCurrentLocation || isFetchingWeatherInformation || isFetchingForecastHistory) {
+        return <div>LOADING...</div>;
+    }
+
     const {
-        line,
-        spanText29,
-        spanText30,
-        line6,
-        spanText31,
-        line7,
-        spanText32,
-        line8,
-        spanText33,
-        line9,
-        spanText34,
-        line10,
-        spanText35,
-        line11,
-        spanText36,
-        spanText37,
-        spanText38,
-        spanText39,
-        spanText40,
-        spanText41,
-        spanText42,
-        spanText43,
-        spanText44,
-        spanText45,
-        spanText46,
-        spanText47,
-        spanText48,
-        spanText49,
-        spanText50,
-        spanText51,
-        spanText52,
-        spanText53,
-        spanText54,
-        spanText55,
-        spanText56,
-        spanText57,
-        spanText58,
-        spanText59,
-        spanText60,
-        spanText61,
-        spanText62,
-        spanText63,
-        spanText64,
-        weekIcons,
         spanText65,
         spanText66,
     } = props;
+
     return (
         <div className="container-center-horizontal">
             <div className="x2 screen">
 
-                <img className="line-23" src={line} alt="Line" />
+                <img className="line-23" alt="Line" />
                 <div className="flex-row-13">
-                    <CurrentConditionsComponent {...weatherMainData}/>
+                    <CurrentConditionsComponent/>
                     <DetailsComponent />
                 </div>
                 <div className="frame-container50">
-                    <div className="frame-50">
-                        <div className="overlap-group50 inter-extra-bold-black-14px">
-                            <div className="overlap-group51">
-                                <div className="title-7-day-forecast inter-bold-black-20px">
-                                    <span className="inter-bold-black-20px">{spanText29}</span>
-                                </div>
-                                <div className="today-2 inter-normal-black-14px">
-                                    <span className="inter-normal-black-14px">{spanText30}</span>
-                                </div>
-                                <img className="line-6" src={line6} alt="Line 6" />
-                                <div className="tue inter-normal-black-14px">
-                                    <span className="inter-normal-black-14px">{spanText31}</span>
-                                </div>
-                                <img className="line50" src={line7} alt="Line 7" />
-                                <div className="wed inter-normal-black-14px">
-                                    <span className="inter-normal-black-14px">{spanText32}</span>
-                                </div>
-                                <img className="line50" src={line8} alt="Line 8" />
-                                <div className="thu inter-normal-black-14px">
-                                    <span className="inter-normal-black-14px">{spanText33}</span>
-                                </div>
-                                <img className="line-51" src={line9} alt="Line 9" />
-                                <div className="fri inter-normal-black-14px">
-                                    <span className="inter-normal-black-14px">{spanText34}</span>
-                                </div>
-                                <img className="line-51" src={line10} alt="Line 10" />
-                                <div className="sat inter-normal-black-14px">
-                                    <span className="inter-normal-black-14px">{spanText35}</span>
-                                </div>
-                                <img className="line-11" src={line11} alt="Line 11" />
-                                <div className="place-2 inter-normal-black-14px">
-                                    <span className="inter-normal-black-14px">{spanText36}</span>
-                                </div>
-                            </div>
-                            <div className="x1day-temp">
-                                <span className="inter-extra-bold-black-14px">{spanText37}</span>
-                                <span className="inter-medium-black-14px">{spanText38}</span>
-                                <span className="inter-bold-black-14px">{spanText39}</span>
-                            </div>
-                            <div className="x2day-temp">
-                                <span className="inter-extra-bold-black-14px">{spanText40}</span>
-                                <span className="inter-medium-black-14px">{spanText41}</span>
-                                <span className="inter-bold-black-14px">{spanText42}</span>
-                            </div>
-                            <div className="x3day-temp">
-                                <span className="inter-extra-bold-black-14px">{spanText43}</span>
-                                <span className="inter-medium-black-14px">{spanText44}</span>
-                                <span className="inter-bold-black-14px">{spanText45}</span>
-                            </div>
-                            <div className="x4day-temp">
-                                <span className="inter-extra-bold-black-14px">{spanText46}</span>
-                                <span className="inter-medium-black-14px">{spanText47}</span>
-                                <span className="inter-bold-black-14px">{spanText48}</span>
-                            </div>
-                            <div className="x5day-temp">
-                                <span className="inter-extra-bold-black-14px">{spanText49}</span>
-                                <span className="inter-medium-black-14px">{spanText50}</span>
-                                <span className="inter-bold-black-14px">{spanText51}</span>
-                            </div>
-                            <div className="x6day-temp">
-                                <span className="inter-extra-bold-black-14px">{spanText52}</span>
-                                <span className="inter-medium-black-14px">{spanText53}</span>
-                                <span className="inter-bold-black-14px">{spanText54}</span>
-                            </div>
-                            <div className="x7day-temp">
-                                <span className="inter-extra-bold-black-14px">{spanText55}</span>
-                                <span className="inter-medium-black-14px">{spanText56}</span>
-                                <span className="inter-bold-black-14px">{spanText57}</span>
-                            </div>
-                            <div className="sunny">
-                                <span className="inter-extra-bold-black-14px">{spanText58}</span>
-                            </div>
-                            <div className="cloudy">
-                                <span className="inter-extra-bold-black-14px">{spanText59}</span>
-                            </div>
-                            <div className="cloudy-1">
-                                <span className="inter-extra-bold-black-14px">{spanText60}</span>
-                            </div>
-                            <div className="rainy">
-                                <span className="inter-extra-bold-black-14px">{spanText61}</span>
-                            </div>
-                            <div className="rainy-1">
-                                <span className="inter-extra-bold-black-14px">{spanText62}</span>
-                            </div>
-                            <div className="rainy-2">
-                                <span className="inter-extra-bold-black-14px">{spanText63}</span>
-                            </div>
-                            <div className="cloudy-2">
-                                <span className="inter-extra-bold-black-14px">{spanText64}</span>
-                            </div>
-                            <img className="week-icons" src={weekIcons} alt="week icons" />
-                        </div>
-                    </div>
+                    <DayForecast />
                     <div className="frame-51">
                         <div className="overlap-group51">
                             <div className="overlap-group-52">
--- weather-app/client/src/js/fetchForecastHistory.js
+++ weather-app/client/src/js/fetchForecastHistory.js
@@ -11,7 +11,7 @@ export const fetchForecastHistoryByIp = (startDate, endDate) => {
             console.log("Canceling dispatch start or end date is null.");
             return;
         }
-        console.log("REQUEST: " + `${LOCAL_API_URL}/history/coordinates?start=` + startDate + "&end=" + endDate);
+        console.log("REQUEST: " + `${LOCAL_API_URL}/history/?start=` + startDate + "&end=" + endDate);
         try {
             // const ip = await dispatch(fetchCurrentLocationByIp());
             const state = await store.getState();
@@ -41,6 +41,36 @@ export const fetchForecastHistoryByCoordinates = (latitude, longitude, startDate
         console.log("REQUEST: " + `${LOCAL_API_URL}/history/coordinates?lat=` + latitude + "&lon=" + longitude + "&start=" + startDate + "&end=" + endDate);
         try {
             const response = await axios.get(`${LOCAL_API_URL}/history/coordinates?lat=` + latitude + "&lon=" + longitude + "&start=" + startDate + "&end=" + endDate); // Perform the asynchronous operation to fetch the location
+            const forecast = response.data; // Extract the location data from the response
+            console.log("Retrieved Data! Current Forecast History: ", forecast);
+            dispatch(setForecastHistory(forecast)); // Dispatch an action to set the fetched location state in Redux
+        } catch (error) {
+            console.error("Error fetching weather information:", error);
+            dispatch(setForecastHistory(null)); // Set the fetching flag to false in case of an error
+        } finally {
+            dispatch(setIsFetchingForecastHistory(false));
+        }
+    };
+};
+
+export const fetchForecastHistoryByCoordinatesAhead = (latitude, longitude, startDate, endDate) => {
+    return async (dispatch) => {
+        dispatch(setIsFetchingForecastHistory(true)); // Set the fetching flag to true to indicate the state is being fetched
+        if (!latitude || !longitude) {
+            dispatch(setIsFetchingForecastHistory(false));
+            console.log("Canceling dispatch latitude or longitude is null.");
+            return;
+        }
+        // const [year, month, day] = endDate.split("-");
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, "0");
+        const day = String(today.getDate() + 13).padStart(2, "0");
+        const newEndDate = `${year}-${month}-${day}`;
+        console.log(`NEW END DATE: ${newEndDate}`);
+        console.log("REQUEST: " + `${LOCAL_API_URL}/history/coordinates?lat=` + latitude + "&lon=" + longitude + "&start=" + startDate + "&end=" + newEndDate);
+        try {
+            const response = await axios.get(`${LOCAL_API_URL}/history/coordinates?lat=` + latitude + "&lon=" + longitude + "&start=" + startDate + "&end=" + newEndDate); // Perform the asynchronous operation to fetch the location
             const forecast = response.data; // Extract the location data from the response
             console.log("Retrieved Data! Current Forecast History: ", forecast);
             dispatch(setForecastHistory(forecast)); // Dispatch an action to set the fetched location state in Redux
