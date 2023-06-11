e21b330082aa7de50cc133eee2defcacd38e8649
Sebastian Termen <Sebastian.Termen@gmail.com>
2023-06-11 15:54:39 +0300

    Changed APIs so that infinite amount of IP calls can be made.

--- weather-app/client/src/env/env.js
+++ weather-app/client/src/env/env.js
@@ -2,3 +2,5 @@
 export const LOCAL_API_IP = "localhost";
 export const LOCAL_API_PORT = "8080";
 export const LOCAL_API_URL = "http://" + LOCAL_API_IP + ":" + LOCAL_API_PORT;
+export const IP_API_1 = "https://ipapi.co/json";
+export const IP_API_2 = "https://api.ipify.org?format=json";
